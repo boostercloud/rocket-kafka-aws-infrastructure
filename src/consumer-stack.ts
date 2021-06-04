83bd840f0ee7fdc6b90115e3f115214dab3bdd25
charlietfe <carlos@theagilemonkeys.com>
2021-06-04 10:45:42 +0100

    added latest cdk

--- src/consumer-stack.ts
+++ src/consumer-stack.ts
@@ -1,17 +1,19 @@
-import { CfnInclude, Duration, Stack } from '@aws-cdk/core'
+import { Duration, Stack } from '@aws-cdk/core'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { PolicyStatement } from '@aws-cdk/aws-iam'
-import { Code, Function, Runtime } from '@aws-cdk/aws-lambda'
+import { Code, Function, Runtime, StartingPosition } from '@aws-cdk/aws-lambda'
 import { Table } from '@aws-cdk/aws-dynamodb'
 import path = require('path')
 import { KafkaRocketParams } from './types'
+import { AuthenticationMethod, SelfManagedKafkaEventSource } from '@aws-cdk/aws-lambda-event-sources';
+import { Secret } from '@aws-cdk/aws-secretsmanager'
 
 export class KafkaConsumerStack {
   static mountStack(stack: Stack, config: BoosterConfig, params: KafkaRocketParams): void {
     const eventsStore = stack.node.tryFindChild('events-store') as Table
 
     const kafkaTriggerFunction = new Function(stack, 'kafkaConsumer', {
-      runtime: Runtime.NODEJS_12_X,
+      runtime: Runtime.NODEJS_14_X,
       timeout: Duration.minutes(15),
       memorySize: 1024,
       handler: 'index.consumerHandler',
@@ -42,37 +44,15 @@ export class KafkaConsumerStack {
     params: KafkaRocketParams,
     kafkaTriggerFunction: Function
   ): void {
-    const baseProperties = {
-      FunctionName: kafkaTriggerFunction.functionArn,
-      StartingPosition: 'LATEST',
-      SelfManagedEventSource: {
-        Endpoints: {
-          KafkaBootstrapServers: params.bootstrapServers,
-        },
-      },
-      SourceAccessConfigurations: [
-        {
-          Type: 'SASL_SCRAM_512_AUTH',
-          URI: params.secretArn,
-        },
-      ],
-    }
-
     params.consumerConfig.forEach((config) => {
-      const resourceName = 'kfkes' + config.topicName.replace(/-/g, '')
-      new CfnInclude(stack, resourceName, {
-        template: {
-          Resources: {
-            [resourceName]: {
-              Type: 'AWS::Lambda::EventSourceMapping',
-              Properties: {
-                ...baseProperties,
-                Topics: [config.topicName],
-              },
-            },
-          },
-        },
-      })
+      kafkaTriggerFunction.addEventSource(new SelfManagedKafkaEventSource({
+        bootstrapServers: params.bootstrapServers,
+        topic: config.topicName,
+        authenticationMethod: AuthenticationMethod.SASL_SCRAM_512_AUTH,
+        batchSize: 100,
+        startingPosition: StartingPosition.LATEST,
+        secret: Secret.fromSecretCompleteArn(stack, 'secret', params.secretArn)
+      }));
     })
   }
 }
